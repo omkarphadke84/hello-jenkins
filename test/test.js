df4089ae01e7aafc26c6cfb3845f3995fc42e09b
Sampada Phadke <Sampada@Sampadas-MacBook-Air.local>
2016-08-31 12:10:41 +0530

    Add node app

---
+++ test/test.js
@@ -0,0 +1,10 @@
+#!/usr/local/bin/node
+var request = require('supertest');
+var app = require('../app.js');
+ 
+describe('GET /', function() {
+  it('respond with hello world', function(done) {
+    request(app).get('/').expect('hello world', done);
+  });
+});
+
