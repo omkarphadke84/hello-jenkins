def0ac61e93489822efbf25ac6323ca83b4b4140
Jenkins <jenkins@ip-172-31-17-14.ap-south-1.compute.internal>
2016-09-02 06:11:06 +0000

    some changes in the test script

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world', done);
+    request(app).get('/').expect('hello jenkins', done);
   });
 });
 
