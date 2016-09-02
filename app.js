18f4dceaede94847317265afd49c39053b671a58
Sampada Phadke <Sampada@Sampadas-MacBook-Air.local>
2016-09-02 11:42:50 +0530

    app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello jenkins');
+  res.send('hello jenk');
 });
  
 app.listen(process.env.PORT || 5000);
